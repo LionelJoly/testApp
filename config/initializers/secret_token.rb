fcc7c12f67c65dae7a80c7d970e30a70ad4b4ffe
lionel <lio.joly@orange.fr>
2011-10-13 20:36:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edi::Application.config.secret_token = '93a98fdf4e51726b05c786a8cb4e1702eea5fd7ce5b7255021334662842eff1591cadaec295b38bceb713f1e80d9042a5f478c46faf1069360ebcfcef3d3ed9d'
